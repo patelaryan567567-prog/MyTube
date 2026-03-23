3b92048579812cf1168281606eb5d181e213d444
Aryan Singh <aryansingh912805@gmail.com>
2026-03-23 11:41:03 +0530

    Add API caching to save quota

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -5,37 +5,32 @@ const BASE_URL = "https://www.googleapis.com/youtube/v3";
 
 const api = axios.create({ baseURL: BASE_URL });
 
+const cache = {};
+const cachedGet = async (url, params) => {
+  const key = url + JSON.stringify(params);
+  if (cache[key]) return cache[key];
+  const res = await api.get(url, { params });
+  cache[key] = res;
+  return res;
+};
+
 export const getTrending = () =>
-  api.get("/videos", {
-    params: { part: "snippet,statistics", chart: "mostPopular", maxResults: 20, regionCode: "IN", key: API_KEY },
-  });
+  cachedGet("/videos", { part: "snippet,statistics", chart: "mostPopular", maxResults: 20, regionCode: "IN", key: API_KEY });
 
 export const searchVideos = (query) =>
-  api.get("/search", {
-    params: { part: "snippet", q: query, maxResults: 20, key: API_KEY },
-  });
-
-export const getChannelVideos = (channelId) =>
-  api.get("/search", {
-    params: { part: "snippet", channelId, type: "video", maxResults: 20, order: "date", key: API_KEY },
-  });
+  cachedGet("/search", { part: "snippet", q: query, maxResults: 20, key: API_KEY });
 
 export const getVideoById = (id) =>
-  api.get("/videos", {
-    params: { part: "snippet,statistics", id, key: API_KEY },
-  });
+  cachedGet("/videos", { part: "snippet,statistics", id, key: API_KEY });
 
 export const getRelatedVideos = (query) =>
-  api.get("/search", {
-    params: { part: "snippet", q: query, type: "video", maxResults: 15, key: API_KEY },
-  });
+  cachedGet("/search", { part: "snippet", q: query, type: "video", maxResults: 15, key: API_KEY });
 
 export const getChannelDetails = (channelId) =>
-  api.get("/channels", {
-    params: { part: "snippet,statistics", id: channelId, key: API_KEY },
-  });
+  cachedGet("/channels", { part: "snippet,statistics", id: channelId, key: API_KEY });
 
 export const getVideosByCategory = (categoryId) =>
-  api.get("/videos", {
-    params: { part: "snippet,statistics", chart: "mostPopular", videoCategoryId: categoryId, maxResults: 20, regionCode: "IN", key: API_KEY },
-  });
+  cachedGet("/videos", { part: "snippet,statistics", chart: "mostPopular", videoCategoryId: categoryId, maxResults: 20, regionCode: "IN", key: API_KEY });
+
+export const getChannelVideos = (channelId) =>
+  cachedGet("/search", { part: "snippet", channelId, type: "video", maxResults: 20, order: "date", key: API_KEY });
